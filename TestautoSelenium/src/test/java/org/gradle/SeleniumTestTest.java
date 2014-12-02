1f4860fcec8a621a728c7e3507a8ed6dff6c5a19
Matthias Karl <itgkarl+git@gmail.com>
2014-12-02 08:26:27 -0800

    fixed delete bug. Generierten Testfall hinzugefügt

---
+++ TestautoSelenium/src/test/java/org/gradle/SeleniumTestTest.java
@@ -0,0 +1,87 @@
+package org.gradle;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SeleniumTestTest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://localhost:3000/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testSeleniumTest() throws Exception {
+    driver.get(baseUrl + "/welcome/index");
+    driver.findElement(By.id("bEnter")).click();
+    driver.findElement(By.linkText("New")).click();
+    driver.findElement(By.id("todo_title")).clear();
+    driver.findElement(By.id("todo_title")).sendKeys("test");
+    driver.findElement(By.id("todo_notes")).clear();
+    driver.findElement(By.id("todo_notes")).sendKeys("test");
+    driver.findElement(By.name("commit")).click();
+    driver.findElement(By.linkText("Back")).click();
+    driver.findElement(By.xpath("(//a[contains(text(),'Delete')])[6]")).click();
+    assertTrue(closeAlertAndGetItsText().matches("^Are you sure[\\s\\S]$"));
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
